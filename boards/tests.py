e2d663761d093b3d19041646ff3e200caf704fce
Bakley <barclaykoin@gmail.com>
2017-09-24 18:53:34 +0300

    Changes on links

--- boards/tests.py
+++ boards/tests.py
@@ -9,19 +9,32 @@ from .models import Board
 
 
 class HomeTests(TestCase):
+    """Home Views test"""
+
+    def setup(self):
+        """The method will create a Home instance, to use in the test"""
+        self.board = Board.objects.create(name='Materialize CSS', description='A beautiful way to create')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_home_view_status_code(self):
         """We are testing the status code of the response."""
-        url = reverse('home')
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         """The test will make sure the URL /, which is the root URL, is returning the home view."""
         view = resolve('/')
         self.assertEquals(view.func, home)
 
+    def test_home_view_contains_links_to_topics_page(self):
+        """Test if The homepage has a link to take the visitor to the topics page of a given Board"""
+        boards_topic_url = reverse('boards_topic', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(boards_topic_url))  # test if the response body contains a given text
+
 
 class BoardsTopicTest(TestCase):
+    """Boards Topic view test"""
+
     def setup(self):
         """The method, will created a Board instance, so to use it in the tests."""
         Board.objects.create(name='Materialize CSS', description='A beautiful way to create')
@@ -42,3 +55,9 @@ class BoardsTopicTest(TestCase):
         """The test will make sure the boards_topic URL, is returning the boards_topic view."""
         view = resolve('/boards/1/')
         self.assertEquals(view.func, boards_topic)
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('boards_topic', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
